b1bc4b4150568a4350e64d37141b5822c62ff961
hyeon <zip235789@gmail.com>
2023-04-21 19:12:51 +0900

    23.04.21. Chapter08-2

---
+++ Chapter08/Chapter08-2/compare_func01.py
@@ -0,0 +1,41 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+    
+    def get_sum(self):
+        return self.korean + self.math +\
+            self.english + self.science
+    
+    def get_average(self):
+        return self.get_sum() / 4
+    
+    def __str__(self):
+        return "{}\t{}\t{}".format(
+            self.name,
+            self.get_sum(),
+            self.get_average())
+    
+    def __eq__(self, value):
+        if not isinstance(value, Student):
+            raise TypeError("Student 클래스의 인스턴스만 비교할 수 있습니다.")
+        return self.get_sum() == value.get_sum()
+    def __ne__(self, value):
+        return self.get_sum() != value.get_sum()
+    def __gt__(self, value):
+        return self.get_sum() > value.get_sum()
+    def __ge__(self, value):
+        return self.get_sum() >= value.get_sum()
+    def __lt__(self, value):
+        return self.get_sum() < value.get_sum()
+    def __le__(self, value):
+        return self.get_sum() <= value.get_sum()
+
+# 학생을 선언합니다.
+student_a = Student("윤인성", 87, 98, 88, 95)   
+
+# 비교합니다.
+student_a == 10
